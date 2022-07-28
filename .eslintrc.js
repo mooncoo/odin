c5d77a9b0d74dc688a4b01fe124109906731d3ba
郭赛赛 <guosaisai@wanmi.com>
2022-07-28 16:02:09 +0800

    feat:调整

--- .eslintrc.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable */
-module.exports = {
-  "extends": ['selling/taro'],
-  "rules": {
-    "no-require-imports": true,
-    "no-console": false
-  }
-}
--- .stylelintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  "extends": "stylelint-config-selling",
-  "rules": {
-    "selector-type-no-unknown": null,
-    "selector-pseudo-class-allowed-list": null
-  }
-}
